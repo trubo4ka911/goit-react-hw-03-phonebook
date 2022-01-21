61b41eed239b48c1563d36e344ddb0f54bdee284
Anna Trubnikova <trubo4ka911@gmail.com>
2022-01-21 14:41:47 +0200

    Update Phonebook.js

--- src/components/Phonebook/Phonebook.js
+++ src/components/Phonebook/Phonebook.js
@@ -9,40 +9,43 @@ import Filter from "../Filter/Filter";
 class Phonebook extends Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
     filter: "",
   };
-
+  componentDidMount = () => {
+    const contacts = localStorage.getItem("contacts")
+      ? JSON.parse(localStorage.getItem("contacts"))
+      : [];
+    this.setState({ contacts });
+  };
   formSubmitHandler = ({ name, number }) => {
-    const contact = {
-      id: nanoid(),
-      name: name,
-      number: number,
+    const newContact = {
+      id: nanoid(5),
+      name: name.toLowerCase(),
+      number,
     };
-    const normalizeName = contact.name.toLowerCase();
     if (
       this.state.contacts.some(
-        (item) => item.name.toLowerCase() === normalizeName
+        (item) => item.name.toLowerCase() === newContact.name
       )
     ) {
-      return toast.error(`${contact.name} is already in your contacts`);
+      return toast.error(`${newContact.name} is already in your contacts`);
     }
-    this.setState((prevState) => ({
-      contacts: [
-        ...prevState.contacts,
-        { id: nanoid(5), name: name, number: number },
-      ],
-    }));
-    return toast.success(`Contact ${contact.name} added successfully!`);
+    const updateContacts = [...this.state.contacts, newContact];
+    this.setState({ contacts: updateContacts });
+    localStorage.setItem("contacts", JSON.stringify(updateContacts));
+    return toast.success(`Contact ${newContact.name} added successfully!`);
   };
   clickDelete = (e) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((contact) => contact.id !== e),
-    }));
+    const updateContacts = this.state.contacts.filter(
+      (contact) => contact.id !== e
+    );
+    this.setState({ contacts: updateContacts });
+    localStorage.setItem("contacts", JSON.stringify(updateContacts));
     return toast.info(`Contact deleted!`);
   };
 
